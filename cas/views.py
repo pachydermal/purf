ced3f4b379c09e4e862eb4e46dc13c8aff20a1a8
Qinlan Shen <qshen@princeton.edu>
2014-03-31 13:31:39 -0400

    Attempt to get cas to do our own stuff

--- cas/views.py
+++ cas/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 import CASClient, os
 
 def index(request):
+    C = CASClient.CASClient()
+    netid = C.Authenticate()
+    print "Content-Type; text/html"
+    print ""
+
     return render(request, 'index.html')
