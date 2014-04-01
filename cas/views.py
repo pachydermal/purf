e804ea46fabb2c15d7f3e21afeb2689a43a8196b
Qinlan Shen <qshen@princeton.edu>
2014-04-01 18:08:54 -0400

    Added venv virtual environment

--- cas/views.py
+++ cas/views.py
@@ -1,11 +1,8 @@
 #!/usr/local/python/current/bin/python
+import _ssl;_ssl.PROTOCOL_SSLv23 = _ssl.PROTOCOL_SSLv3
+
 from django.shortcuts import render
 import CASClient, os
 
 def index(request):
-    C = CASClient.CASClient()
-#    netid = C.Authenticate()
-    print "Content-Type; text/html"
-    print ""
-
     return render(request, 'index.html')
