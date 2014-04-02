a69864bca79c72b36c61a1b9bb7d1328ac4c696b
P <pallavigk@gmail.com>
2014-04-02 18:16:49 -0400

    Attempting to reconcile Heroku server problems

--- purf/settings.py
+++ purf/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+# Parse database configuration from $DATABASE_URL
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -59,10 +61,10 @@ WSGI_APPLICATION = 'purf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydatabase',
+        'NAME': 'prof',
 		'USER': 'postgres',
 		'PASSWORD': 'pass',
-		'HOST': '',
+		'HOST': '127.0.0.1',
     }
 }
 
@@ -83,4 +85,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
