cce60b443687ac9767368c2c3506a5b850e57e0f
Qinlan Shen <qshen@princeton.edu>
2014-04-08 22:51:05 -0400

    Updated database structure to fit design document

--- purf_app/admin.py
+++ purf_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from purf_app.models import Student, Professor
+from purf_app.models import Student, Professor, User, Rating, Project
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'email')
@@ -7,5 +7,17 @@ class StudentAdmin(admin.ModelAdmin):
 class ProfAdmin(admin.ModelAdmin):
     list_display = ('name', 'title', 'email')
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('student', 'professor', 'first_access')
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('professor', 'overall')
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('professor', 'student_name', 'project_title')
+
 admin.site.register(Professor, ProfAdmin)
 admin.site.register(Student, StudentAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(Project, ProjectAdmin)
--- purf_app/models.py
+++ purf_app/models.py
@@ -2,51 +2,11 @@ import datetime
 from django.utils import timezone
 from django.db import models
 
-#REMOVE BLANK=TRUES FOR NEEDED FIELDS plz
-
-DEPTS = (
-    ('ANT', 'Anthropology'),
-    ('ARC', 'Architecture'),
-    ('ART', 'Art & Archaeology'),
-    ('AST', 'Astrophysical Sciences'),
-    ('CBE', 'Chemical and Biological Engineering'),
-    ('CHM', 'Chemistry'),
-    ('CEE', 'Civil and Environmental Engineering'),
-    ('CLA', 'Classics'),
-    ('COM', 'Comparative Literature'),
-    ('COS', 'Computer Science'),
-    ('EAS', 'East Asian Studies'),
-    ('EEB', 'Ecology and Evolutionary Biology'),
-    ('ECO', 'Economics'),
-    ('ELE', 'Electrical Engineering'),
-    ('ENG', 'English'),
-    ('FRE', 'French and Italian'),
-    ('GEO', 'Geosciences'),
-    ('GER', 'German'),
-    ('HIS', 'History'),
-#    ('IND', 'Independent Concentration'),
-    ('MAT', 'Mathematics'),
-    ('MAE', 'Mechanical and Aerospace Engineering'),
-    ('MOL', 'Molecular Biology'),
-    ('MUS', 'Music'),
-    ('NES', 'Near Eastern Studies'),
-    ('ORF', 'Operations Research and Financial Engineering'),
-    ('PHI', 'Philosophy'),
-    ('PHY', 'Physics'),
-    ('POL', 'Politics'),
-    ('PSY', 'Psychology'),
-    ('REL', 'Religion'),
-    ('SLA', 'Slavic Languages and Literatures'),
-    ('SOC', 'Sociology'),
-    ('SPA', 'Spanish and Portuguese'),
-    ('WWS', 'Woodrow Wilson School'),
-)
-
 class Professor(models.Model):
     name = models.CharField(max_length=200)
     title = models.CharField(max_length=200,blank=True)
-    department = models.CharField(max_length=200, choices=DEPTS)
-    image = models.ImageField('Profile Pic', upload_to='images/', blank=True, null=True)
+    department = models.CharField(max_length=200)
+    image = models.ImageField('Profile Pic',upload_to='images/',blank=True,null=True)
     email = models.EmailField()
     office = models.CharField(max_length=200,blank=True)
     phone = models.CharField(max_length=200,blank=True)
@@ -56,31 +16,49 @@ class Professor(models.Model):
     research_links = models.TextField(blank=True)
     website_link = models.URLField(blank=True)
     description = models.TextField(blank=True)
-    max_capacity = models.PositiveIntegerField(blank=True,null=True,default=0)
-    current_capacity = models.PositiveIntegerField(blank=True,null=True,default=0)
-    #previous advisees
-    #rating
-    #pub_date = models.DateTimeField('date published')
+    full = models.BooleanField(default=False)
+    advisees = models.ManyToManyField('Student',blank=True,null=True,default=None)
     def __unicode__(self):
         return self.name
-    def __title__(self):
-        return self.title
-    def __email__(self):
-        return self.email
 
 class Student(models.Model):
-    professor = models.ForeignKey(Professor,blank=True,null=True, default=None)
     name = models.CharField(max_length=200)
-    department = models.CharField(max_length=200, choices=DEPTS)
-    image = models.ImageField('Profile Pic', upload_to='images/', blank=True, null=True)
+    department = models.CharField(max_length=200)
     year = models.PositiveIntegerField()
+    image = models.ImageField('Profile Pic',upload_to='images/',blank=True,null=True)
     email = models.EmailField()
     website_link = models.URLField(blank=True)
     resume_link = models.URLField(blank=True)
     description = models.TextField(blank=True)
-    #prev prof
-    #certs
+    previous_professors = models.ManyToManyField('Professor',blank=True,null=True,default=None)
     research_interests = models.TextField(blank=True)
+    certificates = models.TextField(blank=True)
     def __unicode__(self):  
         return self.name
-        
+       
+class User(models.Model):
+    student = models.ForeignKey('Student',blank=True,null=True,default=None,related_name='controlled_student')
+    professor = models.ForeignKey('Professor',blank=True,null=True,default=None,related_name='controlled_professor')
+    preferences = models.TextField(blank=True)
+    favorited_students = models.ManyToManyField('Student',blank=True,null=True,default=None)
+    favorited_professors = models.ManyToManyField('Professor',blank=True,null=True,default=None)
+    first_access = models.DateTimeField('Time of first access')
+
+class Rating(models.Model):
+    professor = models.ForeignKey('Professor')
+    responsive = models.PositiveIntegerField()
+    frequency = models.PositiveIntegerField()
+    idea_input = models.PositiveIntegerField()
+    overall = models.PositiveIntegerField()
+    comments = models.TextField(blank=True)
+
+class Project(models.Model):
+    professor = models.ForeignKey('Professor')
+    student_name = models.CharField(max_length=200)
+    netid = models.CharField(max_length=200)
+    year = models.PositiveIntegerField()
+    project_title = models.CharField(max_length=500)
+    project_description = models.TextField(blank=True)
+    type_of_project = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    link = models.URLField(blank=True) 
