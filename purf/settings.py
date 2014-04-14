73cd2e7e5e0e64389bde0344d4a0b02eaffff45b
Eugene Lee <eugene@talkto.com>
2014-04-14 00:40:23 -0400

    Cheapo AJAX for search results + Revamped Search page

--- purf/settings.py
+++ purf/settings.py
@@ -116,15 +116,15 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
-
-# Parse database configuration from $DATABASE_URL
-
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+# CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
+#
+# # Parse database configuration from $DATABASE_URL
+#
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+#
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
--- purf_app/static/js/protosearch.js
+++ purf_app/static/js/protosearch.js
@@ -1,7 +1,62 @@
-var search = (function () {
+var search_prof = (function () {
 
+    // the methods that are publicly accessible.
+    // ENSURE THAT NO PRIVATE DATABASE VALUES CAN BE ACCESSED!!
     var public = {};
+    var searchform, searchbox, searchbutton, searchresults;
 
+    // INITIALIZATION
+    init = function () {
+        searchform = $("#search-form")
+        searchbox = $("#searchbox").first();
+        searchbutton =  $("#searchbutton").first();
+        searchresults = $("#search-results")
+
+        // set trigger on enter
+        searchbox.keyup(function(event){
+            if(event.keyCode == 13){
+                $("#id_of_button").click();
+            }
+        });
+
+        // set trigger on button press
+        searchform.submit(function(e) {
+            // submit the form
+            // $(this).ajaxSubmit();
+            search();
+            // return false to prevent normal browser submit and page navigation
+            e.preventDefault();
+            return false;
+        });
+
+    }
+
+    // MAIN SEARCH FUNCTION
+    search = function () {
+        console.log("Searched for " + searchbox.val())
+
+        $.getJSON("api/v1/professor/?format=json", function(data) {
+            searchresults.empty();
+            var items = [];
+            $.each( data.objects, function( key, val ) {
+                items.push(
+                        '<div class="row" style="padding: 10px 0; background:white; margin: 1px 0"> \
+                          <div class="profile col-md-2"> \
+                            <img src="http://lorempixel.com/50/50/people"/> \
+                          </div> \
+                          <div class="name col-md-5">'
+                            + val.name +
+                          '</div> \
+                        </div>'
+                        );
+            });
+
+            searchresults.append(items.join(""))
+        })
+    }
+
+    // on document ready, initialize the function
+    $(init())
 
     return public
 }());
