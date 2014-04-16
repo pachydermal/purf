d09da5d1aa4abdbdd523d688bc4c05c9b96663cb
Eugene Lee <eugene@talkto.com>
2014-04-16 17:00:37 -0400

    Removing venv folder

--- venv/bin/pildriver.py
+++
@@ -1,5 +0,0 @@
-#!/home/qinlan/purf/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'Pillow==2.4.0','pildriver.py'
-__requires__ = 'Pillow==2.4.0'
-import pkg_resources
-pkg_resources.run_script('Pillow==2.4.0', 'pildriver.py')
