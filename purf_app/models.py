9eea4fb3c9d6d023a4184c7da9bd9a8ad492c2d7
Qinlan Shen <qshen@princeton.edu>
2014-04-20 11:51:05 -0400

    Restructured database

--- purf_app/models.py
+++ purf_app/models.py
@@ -3,40 +3,36 @@ from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
-class Professor(models.Model):
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, editable=False,related_name='user_profile')
     name = models.CharField(max_length=200)
-    title = models.CharField(max_length=200,blank=True)
     department = models.CharField(max_length=200)
-    image = models.ImageField('Profile Pic',upload_to='images/',blank=True,null=True)
     email = models.EmailField()
+    image = models.URLField(max_length=200,blank=True)
+    website_link = models.URLField(blank=True)
+    professor_or_student = models.BooleanField(default=False) # professor = True
+
+class Professor(UserProfile):
+    title = models.CharField(max_length=200,blank=True)
     office = models.CharField(max_length=200,blank=True)
     phone = models.CharField(max_length=200,blank=True)
-    WASS = models.URLField(max_length=200,blank=True)
     research_areas = models.TextField(blank=True)
     research_topics = models.TextField(blank=True)
     research_links = models.TextField(blank=True)
-    website_link = models.URLField(blank=True)
     description = models.TextField(blank=True)
     full = models.BooleanField(default=False)
     advisees = models.ManyToManyField('Student',blank=True,null=True,default=None)
-    user = models.OneToOneField(User, editable=False,related_name="professor_user")
     def __unicode__(self):
         return self.name
 
-class Student(models.Model):
-    name = models.CharField(max_length=200)
-    department = models.CharField(max_length=200)
+class Student(UserProfile):
     year = models.PositiveIntegerField()
-    image = models.ImageField('Profile Pic',upload_to='images/',blank=True,null=True)
-    email = models.EmailField()
-    website_link = models.URLField(blank=True)
     resume_link = models.URLField(blank=True)
     description = models.TextField(blank=True)
     previous_professors = models.ManyToManyField('Professor',blank=True,null=True,default=None, related_name='previous_professors')
     research_interests = models.TextField(blank=True)
     certificates = models.TextField(blank=True)
     favorited_professors = models.ManyToManyField('Professor',blank=True,null=True,default=None, related_name='favorited_professors')
-    user = models.OneToOneField(User, editable=False,related_name="student_user")
     def __unicode__(self):  
         return self.name
 '''
