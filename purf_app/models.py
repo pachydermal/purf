5bb0aa2c7c9588a25b5719c2db09ae5c9788b985
stervy <sycognetta@gmail.com>
2014-04-21 02:46:02 -0400

    importing data
    changed profs to have primary key as their netid, and then added tsv
    files, and added an import.py file to import all the data into the
    database

--- purf_app/models.py
+++ purf_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Professor(models.Model):
+    netid = models.CharField(max_length=200, primary_key=True)
     name = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
     email = models.EmailField()
