a65303e199ac58a2a049c6a59e09d0a630564b12
P <pallavigk@gmail.com>
2014-04-21 02:01:30 -0400

    Minor changes

--- purf/settings.py
+++ purf/settings.py
@@ -111,17 +111,17 @@ STATICFILES_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-# CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
+# # CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
 
-# Parse database configuration from $DATABASE_URL
+# # Parse database configuration from $DATABASE_URL
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
--- purf_app/models.py
+++ purf_app/models.py
@@ -40,15 +40,15 @@ class Student(models.Model):
     user = models.OneToOneField(User, editable=False,related_name="student_user")
     def __unicode__(self):  
         return self.name
-'''
-class User(models.Model):
+
+"""class User(models.Model):
     student = models.ForeignKey('Student',blank=True,null=True,default=None,related_name='controlled_student')
     professor = models.ForeignKey('Professor',blank=True,null=True,default=None,related_name='controlled_professor')
     preferences = models.TextField(blank=True)
     #favorited_students = models.ManyToManyField('Student',blank=True,null=True,default=None)
     #favorited_professors = models.ManyToManyField('Professor',blank=True,null=True,default=None)
     first_access = models.DateTimeField('Time of first access')
-'''
+"""
 class Rating(models.Model):
     professor = models.ForeignKey('Professor')
     responsive = models.PositiveIntegerField()
